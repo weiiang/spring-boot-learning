02b47efbc8eba6c75e446e8bc5e5178f4c4b360e
weiqiang <1432114216@qq.com>
2018-04-20 18:27:55 +0800

    update

--- design-pattern-demo/src/main/java/cn/design/pattern/demo/observe/pattern/impl/Wechat.java
+++ design-pattern-demo/src/main/java/cn/design/pattern/demo/observe/pattern/impl/Wechat.java
@@ -1,26 +1,46 @@
 package cn.design.pattern.demo.observe.pattern.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.design.pattern.demo.observe.pattern.Observer;
 import cn.design.pattern.demo.observe.pattern.Observerable;
 
 public class Wechat implements Observerable{
 
-	@Override
-	public void registerObserver(Observer o) {
-		// TODO Auto-generated method stub
-		
-	}
+	//注意到这个List集合的泛型参数为Observer接口，设计原则：面向接口编程而不是面向实现编程
+    private List<Observer> list;
+    private String message;
+    
+    public Wechat() {
+        list = new ArrayList<Observer>();
+    }
+    
+    @Override
+    public void registerObserver(Observer o) {
+        list.add(o);
+    }
+    
+    @Override
+    public void removeObserver(Observer o) {
+        if(!list.isEmpty())
+            list.remove(o);
+    }
 
-	@Override
-	public void removeObserver(Observer o) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void notifyObserver() {
-		// TODO Auto-generated method stub
-		
-	}
+    //遍历
+    @Override
+    public void notifyObserver() {
+        for(int i = 0; i < list.size(); i++) {
+            Observer oserver = list.get(i);
+            oserver.update(message);
+        }
+    }
+    
+    public void setInfomation(String s) {
+        this.message = s;
+        System.out.println("微信服务更新消息： " + s);
+        //消息更新，通知所有观察者
+        notifyObserver();
+    }
 
 }
